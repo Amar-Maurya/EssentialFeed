907e12fd7bfe346f362eb9763eb3eaf9b7ce7fdd
Amar Maurya <amar.maurya-external@tcs.com>
2025-08-31 19:14:00 +0530

    Extract cache insertion logic into helper function

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -22,12 +22,16 @@ class LoadFeedCache {
             if let cacheDeletionError = error {
                 completion(cacheDeletionError)
             } else {
-                self.store.insert(items, timestamp: timestamp) { [weak self] error in
-                    guard self != nil else { return }
-                    completion(error)
-                }
+                self.cache(items, with: completion)
             }
         })
+    }
+    
+    func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+        self.store.insert(items, timestamp: timestamp) { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        }
     }
 }
 
