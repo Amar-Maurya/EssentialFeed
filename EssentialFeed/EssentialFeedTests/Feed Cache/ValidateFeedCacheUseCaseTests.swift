a24981ed6fef54c3fc85d8ca1acc6e2dbfced0ee
Amar Maurya <amar.maurya-external@tcs.com>
2025-09-07 15:55:06 +0530

    create a sperate file for validate the load command to test the cache not deleted on creation

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,27 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by 2674143 on 07/09/25.
+//
+
+import XCTest
+import EssentialFeed
+
+final class ValidateFeedCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.receivedMessage, [])
+    }
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (LocalFeedLoader, FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store,file: file, line: line)
+        trackForMemoryLeaks(sut,file: file, line: line)
+
+        return (sut, store)
+    }
+    
+}
