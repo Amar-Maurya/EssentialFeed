747a3963e950b84d19441804c023c477a898c96a
Amar Maurya <amar.maurya-external@tcs.com>
2025-09-16 07:15:28 +0530

    Added integrated test case file for LocalFeedLoader

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by 2674143 on 16/09/25.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
